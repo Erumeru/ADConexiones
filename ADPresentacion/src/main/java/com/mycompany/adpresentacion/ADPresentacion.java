f4e22b57a2d6453e46c57e1c8b07e6fb90492c6a
Diego Robles Rojas <125101098+Castle373@users.noreply.github.com>
2024-03-12 20:29:18 -0700

    Proyectos Negocio y Presentacion

---
+++ ADPresentacion/src/main/java/com/mycompany/adpresentacion/ADPresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adpresentacion;
+
+/**
+ *
+ * @author diego
+ */
+public class ADPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
